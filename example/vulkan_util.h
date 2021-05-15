bfeae4751dd99870d1d302672bb3fa0b83a319b9
Danil <s48gs.w@gmail.com>
2021-05-15 16:50:25 +0300

    static surface format

--- example/vulkan_util.h
+++ example/vulkan_util.h
@@ -527,6 +527,7 @@ FrameBuffers createFrameBuffers(const VulkanDevice *device, VkSurfaceKHR surface
     colorSpace = surfFormats[0].colorSpace;
     free(surfFormats);
   }
+  colorFormat = VK_FORMAT_B8G8R8A8_UNORM;
 
   // Check the surface capabilities and formats
   VkSurfaceCapabilitiesKHR surfCapabilities;
